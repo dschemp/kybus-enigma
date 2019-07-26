6001b15b502ddc64c0e12b6b69e268d4f5cf6125
Daniel Schemp <annomc1.yt@gmail.com>
2019-07-27 00:42:04 +0200

    Added Benchmark Project and changed test folder structure

---
+++ test/Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KybusEnigma.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NotImplementedException("No Benchmark implemented currently!");
+        }
+    }
+}
