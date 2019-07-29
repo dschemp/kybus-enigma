32aef5c61930117d1b1d3bae2934d8bcbfaf0b15
Daniel Schemp <annomc1.yt@gmail.com>
2019-07-29 11:58:01 +0200

    Added some Official MD5 Test Cases

--- test/xUnit/UnitTestMd5.cs
+++ test/xUnit/UnitTestMd5.cs
@@ -9,6 +9,10 @@ namespace KybusEnigma.xUnit
 {
     public class UnitTestMd5
     {
+        /*
+         * https://tools.ietf.org/html/rfc1321
+         */
+
         public Md5 Md5;
         public TestVectorContainer<string, string> TestVectors;
 
@@ -18,7 +22,13 @@ namespace KybusEnigma.xUnit
             TestVectors = new TestVectorContainer<string, string>(Converter.Text2Bytes, Converter.HexByteDecode)
             {
                 {"Quick Brown Fox", "The quick brown fox jumps over the lazy dog", "9e107d9d372bb6826bd81d3542a419d6"},
-                {"Empty String"   , "", "d41d8cd98f00b204e9800998ecf8427e"}
+                {"Empty String"   , "", "d41d8cd98f00b204e9800998ecf8427e"},
+                {"a" , "a", "0cc175b9c0f1b6a831c399e269772661"},
+                {"abc" , "abc", "900150983cd24fb0d6963f7d28e17f72"},
+                {"message digest" , "message digest", "f96b697d7cb7938d525a2f31aaf161d0"},
+                {"a-z" , "abcdefghijklmnopqrstuvwxyz", "c3fcd3d76192e4007dfb496cca67e13b"},
+                {"A-Za-z0-9" , "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789", "d174ab98d277d9f5a5611c2c9f419d9f"},
+                {"LargeNumber" , "12345678901234567890123456789012345678901234567890123456789012345678901234567890", "57edf4a22be3c955ac49da2e2107b67a"},
             };
         }
 
@@ -39,5 +49,61 @@ namespace KybusEnigma.xUnit
             var hash = Md5.Hash(data);
             CustomAssert.MatchArrays(hash, expected);
         }
+
+        [Fact(DisplayName = "MD5: \"a\"")]
+        public void Md5_SingleSmallA()
+        {
+            var (data, expected) = TestVectors.Get("a");
+
+            var hash = Md5.Hash(data);
+            CustomAssert.MatchArrays(hash, expected);
+        }
+
+        [Fact(DisplayName = "MD5: \"abc\"")]
+        public void Md5_abc()
+        {
+            var (data, expected) = TestVectors.Get("abc");
+
+            var hash = Md5.Hash(data);
+            CustomAssert.MatchArrays(hash, expected);
+        }
+
+        [Fact(DisplayName = "MD5: \"message digest\"")]
+        public void Md5_MessageDigest()
+        {
+            var (data, expected) = TestVectors.Get("message digest");
+
+            var hash = Md5.Hash(data);
+            CustomAssert.MatchArrays(hash, expected);
+        }
+
+        [Fact(DisplayName = "MD5: \"a-z\"")]
+        public void Md5_a2z()
+        {
+            var (data, expected) = TestVectors.Get("a-z");
+
+            var hash = Md5.Hash(data);
+            CustomAssert.MatchArrays(hash, expected);
+        }
+
+        [Fact(DisplayName = "MD5: \"A-Za-z0-9\"")]
+        public void Md5_A2Za2z029()
+        {
+            var (data, expected) = TestVectors.Get("A-Za-z0-9");
+
+            var hash = Md5.Hash(data);
+            CustomAssert.MatchArrays(hash, expected);
+        }
+
+        [Fact(DisplayName = "MD5: \"LargeNumber\"")]
+        public void Md5_LargeNumber()
+        {
+            var (data, expected) = TestVectors.Get("LargeNumber");
+
+            var hash = Md5.Hash(data);
+            CustomAssert.MatchArrays(hash, expected);
+        }
+
+
     }
 }
